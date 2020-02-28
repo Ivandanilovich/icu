4cd609b18b7a8db54dfaccaf05e81bf58f4e5277
Ivan Danilovich <ivandanilovich99@gmail.com>
2020-02-28 21:19:15 +0700

    display images

--- app.py
+++ app.py
@@ -1,12 +1,46 @@
-from flask import Flask
+import base64
+import os
+
+from flask import Flask, render_template, redirect, flash
+from flask_wtf import FlaskForm
+from flask_wtf.csrf import CSRFProtect
+from wtforms import StringField, SubmitField, IntegerField, BooleanField
+from wtforms.validators import DataRequired, NumberRange
 
 app = Flask(__name__)
+app.config.from_object('config')
+
+
+class StartForm(FlaskForm):
+    path = StringField("Path: ", validators=[DataRequired()])
+    class_count = IntegerField("Count: ", validators=[DataRequired(), NumberRange(1, 10)])
+    isRec = BooleanField('is rec')
+    submit = SubmitField("Submit")
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template('first_page.html', form=StartForm())
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
+
+
+def desk(path, classcount):
+    res=[]
+    for i in os.listdir(path)[:5]:
+        with open(os.path.join(path,i), "rb") as image_file:
+            base = base64.b64encode(image_file.read())
+        res.append('''<img src="data:image/png;base64, {}" alt="Red dot" />'''.format(str(base)[2:-1]))
+    return render_template('main.html', form={'path': path, 'classcount': classcount, 'ims': res})
+
+
+@app.route('/main', methods=['POST'])
+def to_main():
+    print('here')
+    form = StartForm()
+    if form.validate_on_submit():
+        flash('Login requested for OpenID="' + form.path.data + '", remember_me=' + str(form.class_count.data))
+        return desk(path=form.path.data, classcount=form.class_count.data)
+    return render_template('first_page.html', form=form)
